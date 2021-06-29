8f9bc0a512b5a007b38be587720ccf9a7e76993b
Guillaume Le Berre <guillaume.le_berre@depinfonancy.net>
2021-06-29 18:14:25 -0400

    improve image scrolling

--- src/app/photo-gallery/photo-gallery.component.ts
+++ src/app/photo-gallery/photo-gallery.component.ts
@@ -1,12 +1,17 @@
-import { HostListener, Component, ElementRef, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { HostListener, Component, ElementRef, OnInit, OnDestroy, AfterViewInit, ViewChild, Output, EventEmitter } from '@angular/core';
 import photos from '../../assets/imgs/meta.json';
+
+
+export interface DialogData {
+  src: string;
+}
 
 @Component({
   selector: 'app-photo-gallery',
   templateUrl: './photo-gallery.component.html',
   styleUrls: ['./photo-gallery.component.css']
 })
-export class PhotoGalleryComponent implements OnInit, AfterViewInit{
+export class PhotoGalleryComponent implements OnInit, AfterViewInit, OnDestroy{
   index: number = 0;
   @ViewChild('col1') col1!: ElementRef;
   @ViewChild('col2') col2!: ElementRef;
@@ -14,20 +19,31 @@ export class PhotoGalleryComponent implements OnInit, AfterViewInit{
   columns_heights = [0, 0, 0];
   columns: ElementRef[] = [];
 
+  private observer!: IntersectionObserver;
+  shortest_column = 0;
+
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
 
-  @HostListener('window:resize', ['$event'])
-  onResize(event: any) {
-    this.updateColumnHeight(0);
-    this.updateColumnHeight(1);
-    this.updateColumnHeight(2);
-    console.log("Width: " + event.target.innerWidth);
+  }
+
+  ngOnDestroy() {
+    
+  }
+
+  @HostListener("window:scroll", [])
+  onScroll(): void {
+    let pos = (document.documentElement.scrollTop || document.body.scrollTop) + document.documentElement.offsetHeight;
+    let max = document.documentElement.scrollHeight;
+    if (max-pos <= 300) {
+      this.addMoreImages(5);
+    }
   }
 
   ngAfterViewInit(): void {
     this.columns = [this.col1, this.col2, this.col3];
+    this.addMoreImages(5);
     return;
   }
 
@@ -43,26 +59,18 @@ export class PhotoGalleryComponent implements OnInit, AfterViewInit{
     return argmin
   }
 
-  updateColumnHeight(i: number): void {
-    let images_in_column = this.columns[i].nativeElement.children;
-    if (images_in_column.length == 0) {
-      this.columns_heights[i] = 0
-    } else {
-      this.columns_heights[i] = images_in_column[images_in_column.length - 1].getBoundingClientRect().bottom;
-    }
-    return;
-  }
-
   public addMoreImages(n: number): void {
     for (let i = 0; i < n; i++) {
       if (this.index >= photos.length) {
         return;
       }
-      let shortest = this.getShortCol();
-      let template = '<a href="' + photos[this.index]["original"] + '" ><img src="' + photos[this.index]["thumbnail"] + '" class= "w-100 shadow rounded mb-4"/></a>'
-      this.columns[shortest].nativeElement.insertAdjacentHTML('beforeend', template);
-      this.index += 1
-      this.updateColumnHeight(shortest)
+      console.log(this.index);
+      let template = '<a href="' + photos[this.index]["original"] + '"><img src="' + photos[this.index]["thumbnail"] + '" class= "w-100 shadow rounded mb-4"/></a>'
+
+      this.columns[this.shortest_column].nativeElement.insertAdjacentHTML('beforeend', template);
+      this.columns_heights[this.shortest_column] += photos[this.index]['ratio'];
+      this.index += 1;
+      this.shortest_column = this.getShortCol();
     }
     return;
   }
--- src/assets/imgs/build_json.py
+++ src/assets/imgs/build_json.py
@@ -1,10 +1,13 @@
 from os import listdir
 from os.path import join
 import json
+from PIL import Image
 
 files = list(listdir("./photos/"))
 data = []
 for f in files:
-  data.append({'original': 'assets/imgs/photos/' + f, 'thumbnail': 'assets/imgs/thumbnails/' + f})
+  im = Image.open('./thumbnails/' + f)
+  width, height = im.size
+  data.append({'original': 'assets/imgs/photos/' + f, 'thumbnail': 'assets/imgs/thumbnails/' + f, 'ratio': float(height)/width})
 
 json.dump(data, open('./meta.json', 'w'))
