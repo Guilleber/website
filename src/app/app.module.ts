575dddfdab19ce13330bd0ec9d1cd4bf084acea2
Guillaume Le Berre <guillaume.le_berre@depinfonancy.net>
2021-06-28 20:43:27 -0400

    move navbar to its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomNavbarComponent } from './custom-navbar/custom-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomNavbarComponent
   ],
   imports: [
     BrowserModule,
