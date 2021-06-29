51c080b025c4735952554f54da87ff1336e3bfc1
Guillaume Le Berre <guillaume.le_berre@depinfonancy.net>
2021-06-28 23:32:20 -0400

    improved routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomNavbarComponent } from './custom-navbar/custom-navbar.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CustomNavbarComponent
+    CustomNavbarComponent,
+    ResumeComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
