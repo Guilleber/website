c1953e40071c840a7a82556a8e875d7c8b42af49
Guillaume Le Berre <guillaume.le_berre@depinfonancy.net>
2021-07-03 17:56:41 -0400

    add --all argument to captioning tool

--- src/assets/imgs/captioning_tool.py
+++ src/assets/imgs/captioning_tool.py
@@ -1,16 +1,23 @@
 import json
 from PIL import Image
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Captioning tool')
+parser.add_argument('--all', action='store_true', help="Whether or not to show all image or only the ones that have an empty caption")
+args = parser.parse_args()
 
 data = json.load(open("meta.json", 'r'))
 prev_caption = ""
 for line in data:
-  im = Image.open("../../" + line['thumbnail'])
-  im.show()
-  caption = input("caption for {}?".format(line['original']))
-  if caption != "":
-    if caption == "same":
-      caption = previous_caption
-    line['caption'] = caption
+  if args.all or line['caption'] != "":
+    im = Image.open("../../" + line['thumbnail'])
+    im.show()
+    caption = input("caption for {}?".format(line['original']))
+    if caption != "":
+      if caption == "same":
+        caption = previous_caption
+      line['caption'] = caption
 
   previous_caption = caption
 
